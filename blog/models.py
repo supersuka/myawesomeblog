429414f3c42305d8d646c447077ac8ef243d5139
Dmitry <dmitrijlukanin8@gmail.com>
2020-12-04 23:54:01 +0500

    project is done

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,8 @@ class Post(models.Model):
 	text = models.TextField()
 	image = models.ImageField(upload_to = 'event_images/')
 		
+	def get_summary(self):
+		return self.text[:70]
+
+	def __str__(self):
+		return self.title
